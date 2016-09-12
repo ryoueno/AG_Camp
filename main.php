8bf69e30503bb79803f40a88a4bb893dbefd5e1d
上野 涼 <ryo@ryo-mac-second.local>
2016-09-13 00:08:26 +0900

    メインボタン実装

--- main.php
+++ main.php
@@ -1,5 +1,15 @@
 <!-- メイン画面 -->
 <div id="main">
-	<h1>これはメインです</h1>
+    <h1>タイトル</h1>
+	<div id="menu">
+		<button type="button" class="btn" name="button" value="button">button1</button>
+		<button type="button" class="btn" name="button" value="button">button2</button>
+		<button type="button" class="btn" name="button" value="button">button3</button>
+		<button type="button" class="btn" name="button" value="button">button4</button>
+		<button type="button" class="btn" name="button" value="button">button5</button>
+		<button type="button" class="btn" name="button" value="button">button6</button>
+		<button type="button" class="btn" name="button" value="button">button7</button>
+		<button type="button" class="btn" name="button" value="button">button8</button>
+	</div>
 </div>
 <!-- /メイン画面 -->
